b7d3880fa052f583250512187febbd0acfca5466
dorwp <dorianwill119@gmail.com>
2025-04-25 19:35:30 -0400

    final

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,9 +6,36 @@
         {
                 return a + b;
         }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool c)
+        {
+            if (c == false)
+            {
+                return (a + b).ToString();
+            }
+
+            if (a + b > 1)
+            {
+                return (a + b).ToString() + " dollars";
+            }
+
+            else 
+            {
+                return (a + b).ToString() + " dollar";
+            }
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1.5, 2.2));
+            Console.WriteLine(Add(1, 2, true));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(1, 3, false));
         }
     }
 }
