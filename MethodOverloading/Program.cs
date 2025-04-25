bbe9ee052303a0e75be956c1c229dad2a7973b2f
dorwp <dorianwill119@gmail.com>
2025-04-25 19:21:34 -0400

    v1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,13 @@
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+                return a + b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
